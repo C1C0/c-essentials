470f4177285650ca45142e6be9da059ec19f90c0
C1C0 <kpskola1002@gmail.com>
2021-09-18 18:24:54 +0200

    2118LAB init

---
+++ Mobule2/2118LAB.c
@@ -0,0 +1,8 @@
+int main()
+{
+	float halfValue = 0.6;
+	float piValue = 3.141 592 65;
+	printf("The value of half is: %f/2\n", half Value);
+	printf("The value of Pi is: %f\n", pi_Value);
+	return 0;
+}
