52e4b6d2e6542f57e90ce805b776d06b624e9d87
C1C0 <kpskola1002@gmail.com>
2021-09-18 17:14:11 +0200

    new init of LABS xD

---
+++ Module1/1138LAB.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("The value of six is: %d \n", 16,0-10-);
+	return 0;
+}
---
+++ Module1/1139LAB.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("The value of seven is: %d \n", 0087);
+	printf("The value of eight is: %d \n", (8000x - 7992x)/x);
+}
