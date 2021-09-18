622b3566832fdf4df7bcc82626a76dc64d4e8e53
C1C0 <kpskola1002@gmail.com>
2021-09-18 17:01:11 +0200

    Lab 4 completed

--- Module1/LAB4-working-with-octal-numbers.c
+++ Module1/LAB4-working-with-octal-numbers.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("The value of nine is: %d \n", 01x);
-	printf("The value of ten is: %d \n", 0x2);
-	return 0;
+int main() {
+  printf("The value of nine is: %d \n", 011);
+  printf("The value of ten is: %d \n", 012);
+  return 0;
 }
