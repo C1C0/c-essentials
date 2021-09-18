82052ef6af93d1c33e8ee85882f3612ab4037731
C1C0 <kpskola1002@gmail.com>
2021-09-18 17:09:58 +0200

    LAb 5 completed

--- Module1/LAB5-working-with-hex.c
+++ Module1/LAB5-working-with-hex.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("The value of twenty-one is: %d \n", 0015);
-	printf("The value of twenty-two is: %d \n", 0x1y);
-	printf("The value of sixty-two is: %d \n", 0x3G);
-	printf("The value of seventy-four is: %d \n", 0x44);
-	return 0;
+int main() {
+  printf("The value of twenty-one is: %d \n", 0x15);
+  printf("The value of twenty-two is: %d \n", 0x16);
+  printf("The value of sixty-two is: %d \n", 0x3E);
+  printf("The value of seventy-four is: %d \n", 0x4a);
+  return 0;
 }
