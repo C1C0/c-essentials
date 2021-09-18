d6cd1723501d27b477c68b1b55b4464e01edac92
C1C0 <kpskola1002@gmail.com>
2021-09-18 18:26:04 +0200

    2118LAB done

--- Mobule2/2118LAB.c
+++ Mobule2/2118LAB.c
@@ -1,8 +1,9 @@
-int main()
-{
-	float halfValue = 0.6;
-	float piValue = 3.141 592 65;
-	printf("The value of half is: %f/2\n", half Value);
-	printf("The value of Pi is: %f\n", pi_Value);
-	return 0;
+#include <stdio.h>
+
+int main() {
+  float halfValue = 0.5;
+  float piValue = 3.14159265;
+  printf("The value of half is: %f\n", halfValue);
+  printf("The value of Pi is: %f\n", piValue);
+  return 0;
 }
