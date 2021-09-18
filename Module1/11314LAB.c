598de12b59c3da4ae39e83c898ac8b3346a2678e
C1C0 <kpskola1002@gmail.com>
2021-09-18 17:21:03 +0200

    11314Lab init

---
+++ Module1/11314LAB.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int ip Part1 = 027;
+	int ip Part2 = 0;
+	int ip Part3 = 0;
+	int ip Part4 = 1;
+
+	printf("Localhost IP is %d.%d.%d.%d\n", ip Part1, ip Part2, ip Part3, ip Part4);
+}
