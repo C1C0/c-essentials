08b25acec8a266ef97406b2e0687a266d1f6db46
C1C0 <kpskola1002@gmail.com>
2021-09-18 18:24:20 +0200

    2117LAB done

--- Mobule2/2117LAB.c
+++ Mobule2/2117LAB.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
-	float tenValue = 10.000000;
-	printf("The value of ten is: %f\n", 09.000);
-	printf("The value of nine is: %f\n", tenValue0);
-	return 0;
+int main() {
+  float tenValue = 10.0;
+  printf("The value of nine is: %f\n", 9.0);
+  printf("The value of ten is: %f\n", tenValue);
+  return 0;
 }
