26908b3ec6068f803f04fd8490aae82fd2a992ff
C1C0 <kpskola1002@gmail.com>
2021-09-20 18:27:48 +0200

    21216LAB done

--- Mobule2/21216LAB.c
+++ Mobule2/21216LAB.c
@@ -2,7 +2,7 @@
 
 int main() {
   int xValue = 4 * 6 % 5;
-  int eightValue = 2 xValue 10 5;
+  int eightValue = 2 * xValue + 10 % 5;
   printf("The value of eight is: %d\n", eightValue);
   return 0;
 }
