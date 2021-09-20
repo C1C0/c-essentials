290d63883f8a6067efe5e5ec8e45db9d542c2926
C1C0 <kpskola1002@gmail.com>
2021-09-20 19:37:32 +0200

    2148 - 21410 init

---
+++ Mobule2/2-1-4-10_LAB.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int dayOfWeek = 1;
+	/* your code */
+	return 0;
+}
---
+++ Mobule2/2-1-4-9_LAB.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n = -3;
+	/* your code */
+	return 0;
+}
