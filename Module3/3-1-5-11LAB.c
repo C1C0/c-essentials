6b01da558c655e485f19c93f53097bb396bdfb45
C1C0 <kpskola1002@gmail.com>
2021-09-23 21:14:47 +0200

    31511 - 31512 done

--- Module3/3-1-5-11LAB.c
+++ Module3/3-1-5-11LAB.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+
+  short int year;
+
+  scanf("%d", &year);
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    printf("%d is a leap year.", year);
+  } else {
+    printf("%d is not a leap year.", year);
+  }
+
+  return 0;
 }
--- Module3/3-1-5-12LAB.c
+++ Module3/3-1-5-12LAB.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+  unsigned char userInput, firstPart, secondPart;
+
+  scanf("%d", &userInput);
+
+  unsigned char nibble1 = 240;
+  firstPart = (userInput & nibble1) >> 4;
+
+  unsigned char nibble2 = 15;
+  secondPart = (userInput & nibble2);
+
+  printf("H nibble: %d", firstPart);
+  printf("\nL nibble: %d", secondPart);
+
+  return 0;
 }
