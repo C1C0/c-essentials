443596465d84cc8eb909fb7ef8dee2adc347aa8e
C1C0 <kpskola1002@gmail.com>
2021-09-27 14:57:33 +0200

    4152 - 4153 - DONE

--- Module4/4-1-5-2LAB.c
+++ Module4/4-1-5-2LAB.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+  char name1[20], name2[20], name3[20], name4[20];
+
+  scanf("%s", &name1);
+  scanf("%s", &name2);
+  scanf("%s", &name3);
+  scanf("%s", &name4);
+
+  printf("%s\n%s\n%s\n%s", name1, name2, name3, name4);
+
+  return 0;
 }
