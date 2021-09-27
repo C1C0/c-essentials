daf8628e57721195170ee35f5cb81bdb7f782cca
C1C0 <kpskola1002@gmail.com>
2021-09-27 14:17:22 +0200

    41210 - 41211 LAB done

--- Module4/4.1.2.10LAB.c
+++ Module4/4.1.2.10LAB.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+  int fibonnaci[10];
+
+  // fill fibonnaci sequence
+  for (int i = 0; i < sizeof(fibonnaci) / sizeof(int); i++) {
+    int number = 1;
+
+    // check, if previous arent out of scope of array
+    if (i - 2 >= 0 && i - 1 > 0) {
+      number = fibonnaci[i - 2] + fibonnaci[i - 1];
+    }
+
+    fibonnaci[i] = number;
+  }
+
+  // print the array
+  for (int i = 0; i < sizeof(fibonnaci) / sizeof(int); i++) {
+    printf("%d\n", fibonnaci[i]);
+  }
+
+  // print only evem positions
+  for (int i = 0; i < sizeof(fibonnaci) / sizeof(int); i++) {
+    if (i % 2 == 0) {
+      printf("%d\n", fibonnaci[i]);
+    }
+  }
+
+  // print only odd positions
+  for (int i = 0; i < sizeof(fibonnaci) / sizeof(int); i++) {
+    if (i % 2 != 0) {
+      printf("%d\n", fibonnaci[i]);
+    }
+  }
+
+  return 0;
 }
--- Module4/4.1.2.11LAB.c
+++ Module4/4.1.2.11LAB.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main(void) {
+  char alphabet[26], c;
+  for (c = 'a'; c <= 'z'; c++) {
+    alphabet[c - 'a'] = c;
+  }
+
+  for (c = 'z'; c > 'a'; c--) {
+    printf("%c\n", alphabet[c - 'a']);
+  }
+  printf("%c", alphabet['g' - 'a']);
+  printf("%c", alphabet['r' - 'a']);
+  printf("%c", alphabet['e' - 'a']);
+  printf("%c", alphabet['a' - 'a']);
+  printf("%c\n", alphabet['t' - 'a']);
+  return 0;
 }
