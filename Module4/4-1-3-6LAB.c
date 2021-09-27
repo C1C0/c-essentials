ae8a4642e5676503fcef7e106dff8ecb1e0ad7bf
C1C0 <kpskola1002@gmail.com>
2021-09-27 14:43:22 +0200

    4136 done

--- Module4/4-1-3-6LAB.c
+++ Module4/4-1-3-6LAB.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
   float numbers[10] = {5.6, 4.3, 6.2, 6.4, 7.3, 2.3, 8.3, 9.2, 0.1, 1.9};
-  int i, swapped;
+  /* your code */
+  int swapped;
+  float aux;
+
   do {
     swapped = 0;
-    for (i = 0; i < 10; i++) {
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(float); i++) {
       if (numbers[i] < numbers[i + 1]) {
         swapped = 1;
-        float number = numbers[i];
+        aux = numbers[i];
         numbers[i] = numbers[i + 1];
-        numbers[i + 1] = number;
+        numbers[i + 1] = aux;
       }
     }
-    for (i = 0; i < 10; i++)
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(float); i++) {
       printf("%.2f ", numbers[i]);
+    }
     printf("\n");
+
   } while (swapped);
 
-  for (i = 0; i < 10; i++)
+  for (int i = 0; i < sizeof(numbers) / sizeof(float); i++) {
     printf("%.2f ", numbers[i]);
+  }
   printf("\n");
+
   return 0;
 }
