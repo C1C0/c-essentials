757bf4bc931f352dcbb6ee69d6e0eaa450b07c34
C1C0 <kpskola1002@gmail.com>
2021-10-13 14:05:42 +0200

    411010LAB done

--- Module4/4-1-10-10LAB.c
+++ Module4/4-1-10-10LAB.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+  char normalWord[50];
+  int i, j;
+  scanf("%s", &normalWord);
+
+  j = 0;
+  for (i = strlen(normalWord); i >= 0; i--) {
+    printf("%c", normalWord[i]);
+
+    if (i == 0) {
+      printf("\n");
+    }
+  }
+
+  return 0;
 }
