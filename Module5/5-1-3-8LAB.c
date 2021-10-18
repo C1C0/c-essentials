b89950dda9bd01531c349eb690a64ebff25aedef
C1C0 <kpskola1002@gmail.com>
2021-10-18 14:30:23 +0200

    5138 their solution

--- Module5/5-1-3-8LAB.c
+++ Module5/5-1-3-8LAB.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 
-int main() {
-  /* your code */
-  char chessBoard[8][8] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                           {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                           {},
-                           {},
-                           {},
-                           {},
-                           {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                           {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
-
-
-    for(int r = 0; r < sizeof(chessBoard) / (sizeof(char) * 8); r++){
-        for(int c = 0; c < sizeof(chessBoard[r]) / sizeof(char); c++){
-            printf("%c", chessBoard[r][c]);
-        }
-
-        printf("\n");
-    }
-
-  return 0;
+int main(void) 
+{
+	char board[8][8];
+	board[0][0] = 'R';
+	board[0][1] = 'N';
+	board[0][2] = 'B';
+	board[0][3] = 'Q';
+	board[0][4] = 'K';
+	board[0][5] = 'B';
+	board[0][6] = 'N';
+	board[0][7] = 'R';
+	board[7][0] = 'R';
+	board[7][1] = 'N';
+	board[7][2] = 'B';
+	board[7][3] = 'Q';
+	board[7][4] = 'K';
+	board[7][5] = 'B';
+	board[7][6] = 'N';
+	board[7][7] = 'R';
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		board[1][i] = 'P';
+		board[2][i] = ' ';
+		board[3][i] = ' ';
+		board[4][i] = ' ';
+		board[5][i] = ' ';
+		board[6][i] = 'P';
+	}
+	int j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", board[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
