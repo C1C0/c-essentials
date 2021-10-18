9acfbb1adb327e0f3664f6e8729c399dcebbe0ea
C1C0 <kpskola1002@gmail.com>
2021-10-18 14:29:54 +0200

    5138 my solution

--- Module5/5-1-3-8LAB.c
+++ Module5/5-1-3-8LAB.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
-	/* your code */
-	return 0;
+int main() {
+  /* your code */
+  char chessBoard[8][8] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                           {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                           {},
+                           {},
+                           {},
+                           {},
+                           {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                           {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+
+
+    for(int r = 0; r < sizeof(chessBoard) / (sizeof(char) * 8); r++){
+        for(int c = 0; c < sizeof(chessBoard[r]) / sizeof(char); c++){
+            printf("%c", chessBoard[r][c]);
+        }
+
+        printf("\n");
+    }
+
+  return 0;
 }
