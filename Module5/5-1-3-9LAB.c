92135a80451993b3b6728727dc214f4696b509c8
C1C0 <kpskola1002@gmail.com>
2021-10-18 14:38:19 +0200

    5139 Done by me

--- Module5/5-1-3-9LAB.c
+++ Module5/5-1-3-9LAB.c
@@ -3,5 +3,19 @@
 int main()
 {
 	/* your code */
+    char day;
+    char *days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+    scanf("%d", &day);
+
+    if(day < 0 || day >= 7){
+        printf("Error, no such day");
+        return -1;
+    }
+
+    printf("Pointer version: %s\n", *(days + day));
+    printf("Array index version: %s", days[day]);
+
+
 	return 0;
 }
