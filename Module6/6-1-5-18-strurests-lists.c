027c58984e23657aca5ddc1b24059b3eb205ea7e
C1C0 <kpskola1002@gmail.com>
2021-11-29 16:32:44 +0100

    init

---
+++ Module6/6-1-5-18-strurests-lists.c
@@ -0,0 +1,51 @@
+/**
+ * @file 6-1-5-18-strurests-lists.c
+ * @author your name (you@domain.com)
+ * @brief Scenario
+Write a program that creates a FIFO queue and prints some values. For this lab, use structures, pointers to structures and the malloc function.
+
+Use the values from the array included in the code fragment. Create one element of a queue for one element of an array (there are ten elements).
+
+After you have created the list, print the first five values of the queue and then the first seven values of the queue. Free up the memory.
+
+After all the operations have been completed, print the message: Memory is freed..
+
+Your version of the program must print the same result as the expected output.
+
+Expected output
+First 5 values
+2
+4
+5
+6
+7
+First 7 values
+2
+4
+5
+6
+7
+8
+9
+Memory is freed
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+	int value;
+	struct element *next;
+};
+int main(void)
+{
+	int values[10] = { 2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
+	/* your code */
+	return 0;
+}
