a4c83dfbee644366559dd3c792588d251494b62e
C1C0 <kpskola1002@gmail.com>
2021-11-29 17:05:18 +0100

    Their solution

--- Module6/6-1-5-18-strurests-lists.c
+++ Module6/6-1-5-18-strurests-lists.c
@@ -1,72 +1,54 @@
-/**
- * @file 6-1-5-18-strurests-lists.c
- * @author your name (you@domain.com)
- * @brief Scenario
-Write a program that creates a FIFO queue and prints some values. For this lab,
-use structures, pointers to structures and the malloc function.
-
-Use the values from the array included in the code fragment. Create one element
-of a queue for one element of an array (there are ten elements).
-
-After you have created the list, print the first five values of the queue and
-then the first seven values of the queue. Free up the memory.
-
-After all the operations have been completed, print the message: Memory is
-freed..
-
-Your version of the program must print the same result as the expected output.
-
-Expected output
-First 5 values
-2
-4
-5
-6
-7
-First 7 values
-2
-4
-5
-6
-7
-8
-9
-Memory is freed
- * @version 0.1
- * @date 2021-11-29
- *
- * @copyright Copyright (c) 2021
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct element {
-  int value;
-  struct element *next;
+struct element
+{
+	int value;
+	struct element *next;
 };
-int main(void) {
-  int values[10] = {2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
-  struct element *fifo =
-      (struct element *)malloc(sizeof(struct element *) * (sizeof values / sizeof(int)));
-
-  struct element *head = fifo;
-	struct element *ptr;
-	int i = 0;
-
-	for(ptr = head; ptr; ptr = ptr->next){
-		ptr->value = values[i];
-		ptr->next = ptr + sizeof(struct element *);
-
-		printf("%d\n", ptr->value);
-
-		i++;
-
-		if(i > sizeof values / sizeof(int)){
-			break;
+int main(void)
+{
+	int values[10] = { 2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
+	int i;
+	struct element * current;
+	struct element * last;
+	struct element * head = (struct element *) malloc(sizeof(struct element));
+	current = head;
+	last = head;
+	for (i=0 ; i < 10 ; i++)
+	{
+		if (current != head)
+		{
+			current = (struct element *) malloc(sizeof(struct element));
+			last->next = current;
 		}
+		current->value = values[i];
+		current->next = NULL;
+		last = current;
+		current = last->next;
 	}
 
-  return 0;
+	current = head;
+	puts("First 5 values");
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d\n", current->value);
+		current = current->next;
+	}
+	current = head;
+	puts("First 7 values");
+	for (i = 0; i < 7; i++)
+	{
+		printf("%d\n", current->value);
+		current = current->next;
+	}
+	current = head;
+	while (current)
+	{
+		last = current->next;
+		free(current);
+		current = last;
+	}
+	puts("Memory is freed");
+	return 0;
 }
