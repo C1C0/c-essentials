da30ce339671ff70d9b12fa6aa43ac4365425aa6
C1C0 <kpskola1002@gmail.com>
2021-11-29 15:59:13 +0100

    My solution

--- Module6/6-1-5-16-Structures.c
+++ Module6/6-1-5-16-Structures.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * @brief Scenario
+Write a program that creates the first names and last names of four people and
+then prints them. Create the appropriate structure.
 
+Then create one variable for each person, initialize their values with a
+constant value (from the code - don't interact with the user this time).
 
-    return 0;
+Finally, print each of them on a separate line.
+
+Your version of the program must print the same result as the expected output.
+
+Expected output
+Mary Smith
+James Johnson
+Patricia Williams
+John Brown
+ *
+ */
+int main() {
+
+  struct PERSON {
+    char *firstName;
+    char *lastName;
+  };
+
+  struct PERSON ms = {"Mary", "Smith"};
+  struct PERSON jj = {"James", "Johnson"};
+  struct PERSON pw = {"Patricia", "Williams"};
+  struct PERSON jb = {"John", "Brown"};
+
+  printf("%s %s\n", ms.firstName, ms.lastName);
+  printf("%s %s\n", jj.firstName, jj.lastName);
+  printf("%s %s\n", pw.firstName, pw.lastName);
+  printf("%s %s\n", jb.firstName, jb.lastName);
+
+  return 0;
 }
