8a5f2355ae2d449995abe036dc59e87f3b6febcb
C1C0 <kpskola1002@gmail.com>
2021-11-29 15:59:36 +0100

    Theri solution

--- Module6/6-1-5-16-Structures.c
+++ Module6/6-1-5-16-Structures.c
@@ -1,40 +1,14 @@
 #include <stdio.h>
 
-/**
- * @brief Scenario
-Write a program that creates the first names and last names of four people and
-then prints them. Create the appropriate structure.
-
-Then create one variable for each person, initialize their values with a
-constant value (from the code - don't interact with the user this time).
-
-Finally, print each of them on a separate line.
-
-Your version of the program must print the same result as the expected output.
-
-Expected output
-Mary Smith
-James Johnson
-Patricia Williams
-John Brown
- *
- */
-int main() {
-
-  struct PERSON {
-    char *firstName;
-    char *lastName;
-  };
-
-  struct PERSON ms = {"Mary", "Smith"};
-  struct PERSON jj = {"James", "Johnson"};
-  struct PERSON pw = {"Patricia", "Williams"};
-  struct PERSON jb = {"John", "Brown"};
-
-  printf("%s %s\n", ms.firstName, ms.lastName);
-  printf("%s %s\n", jj.firstName, jj.lastName);
-  printf("%s %s\n", pw.firstName, pw.lastName);
-  printf("%s %s\n", jb.firstName, jb.lastName);
-
-  return 0;
+int main(void) 
+{
+	struct person person1 = {"Mary", "Smith"};
+	struct person person2 = {"James", "Johnson"};
+	struct person person3 = {"Patricia", "Williams"};
+	struct person person4 = {"John", "Brown"};
+	printf("%s %s\n", person1.firstName, person1.lastName);
+	printf("%s %s\n", person2.firstName, person2.lastName);
+	printf("%s %s\n", person3.firstName, person3.lastName);
+	printf("%s %s\n", person4.firstName, person4.lastName);
+	return 0;
 }
