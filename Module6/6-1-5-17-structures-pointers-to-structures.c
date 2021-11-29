1251e380caceacfd098ecad1c46b424e7c721d61
C1C0 <kpskola1002@gmail.com>
2021-11-29 16:11:56 +0100

    my solution

--- Module6/6-1-5-17-structures-pointers-to-structures.c
+++ Module6/6-1-5-17-structures-pointers-to-structures.c
@@ -41,21 +41,35 @@ int main(void)
 	struct house house2;
 	struct house house3;
 	struct house house4;
+
 	house1.houseNumber = 1;
 	house2.houseNumber = 3;
 	house3.houseNumber = 5;
 	house4.houseNumber = 7;
+
 	house1.next = &house2;
 	house2.next = &house3;
 	house3.next = &house4;
 	house4.next = NULL;
+
 	house1.previous = NULL;
 	house2.previous = &house1;
 	house3.previous = &house2;
 	house4.previous = &house3;
+
 	struct house *firstHouse = &house1;
 	struct house *lastHouse = &house4;
 	struct house *current;
-	/* your code */
+	
+    printf("Ascending order:\n");
+    for(current = firstHouse; current; current = current->next){
+        printf("%d\n", current->houseNumber);
+    }
+
+    printf("Descending order:\n");
+    for(current = lastHouse; current; current = current->previous){
+        printf("%d\n", current->houseNumber);
+    }
+    
 	return 0;
 }
