68b69e98f0369fc2d68a4636f8c0f4329f636acc
C1C0 <kpskola1002@gmail.com>
2021-12-03 09:54:44 +0100

    Init

---
+++ Module6/6-1-7-5-function-parameters.c
@@ -0,0 +1,31 @@
+/**
+ * @file 6-1-7-5-function-parameters.c
+ * @author your name (you@domain.com)
+ * @brief Scenario
+Write a function that computes the square of a given floating-point number and returns its value. Separate the declaration of the function from its full definition.
+
+Write a test code to test the function with some values. Do not limit yourself only to those values we've provided you.
+
+Expected output
+square of 2.00 is 4.00
+square of 6.00 is 36.00
+square of 2.50 is 6.25
+square of 12.12 is 146.89
+square of 345.68 is 119493.29
+ * @version 0.1
+ * @date 2021-12-03
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+
+/* your code */
+
+int main()
+{
+	/* your code */
+	return 0;
+}
+/* your code */
