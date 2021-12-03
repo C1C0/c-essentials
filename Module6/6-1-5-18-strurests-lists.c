ab32ceebd097aa8b7f1cbf602441baeb43be58df
C1C0 <kpskola1002@gmail.com>
2021-12-03 09:51:33 +0100

    Clean up

--- Module6/6-1-5-18-strurests-lists.c
+++ Module6/6-1-5-18-strurests-lists.c
@@ -1,54 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct element
-{
-	int value;
-	struct element *next;
+struct element {
+  int value;
+  struct element *next;
 };
-int main(void)
-{
-	int values[10] = { 2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
-	int i;
-	struct element * current;
-	struct element * last;
-	struct element * head = (struct element *) malloc(sizeof(struct element));
-	current = head;
-	last = head;
-	for (i=0 ; i < 10 ; i++)
-	{
-		if (current != head)
-		{
-			current = (struct element *) malloc(sizeof(struct element));
-			last->next = current;
-		}
-		current->value = values[i];
-		current->next = NULL;
-		last = current;
-		current = last->next;
-	}
+int main(void) {
+  int values[10] = {2, 4, 5, 6, 7, 8, 9, 1, 3, 0};
+  int i;
 
-	current = head;
-	puts("First 5 values");
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d\n", current->value);
-		current = current->next;
-	}
-	current = head;
-	puts("First 7 values");
-	for (i = 0; i < 7; i++)
-	{
-		printf("%d\n", current->value);
-		current = current->next;
-	}
-	current = head;
-	while (current)
-	{
-		last = current->next;
-		free(current);
-		current = last;
-	}
-	puts("Memory is freed");
-	return 0;
+  struct element *current;
+  struct element *last;
+  struct element *head = (struct element *)malloc(sizeof(struct element));
+
+  current = head;
+  last = head;
+
+  for (i = 0; i < 10; i++) {
+    if (current != head) {
+      current = (struct element *)malloc(sizeof(struct element));
+      last->next = current;
+    }
+    current->value = values[i];
+    current->next = NULL;
+    last = current;
+    current = last->next;
+  }
+
+  current = head;
+  puts("First 5 values");
+  for (i = 0; i < 5; i++) {
+    printf("%d\n", current->value);
+    current = current->next;
+  }
+
+  current = head;
+  puts("First 7 values");
+  for (i = 0; i < 7; i++) {
+    printf("%d\n", current->value);
+    current = current->next;
+  }
+
+  current = head;
+  while (current) {
+    last = current->next;
+    free(current);
+    current = last;
+  }
+  
+  puts("Memory is freed");
+  return 0;
 }
