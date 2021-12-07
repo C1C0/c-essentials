ecba8882f18e286b9a206c829fa47945a05eb391
C1C0 <kpskola1002@gmail.com>
2021-12-07 11:48:57 +0100

    Init

---
+++ Module6/6-1-7-7-function-parameters3.c
@@ -0,0 +1,45 @@
+/**
+ * @file 6-1-7-7-function-parameters3.c
+ * @author your name (you@domain.com)
+ * @brief Scenario
+Write a function that checks which of two given matrices is greater. To simplify the function parameter list, you can assume that both matrices are equal in size and are square.
+
+This function should return:
+
+1 if the first matrix is greater than the second;
+-1 if the first matrix is smaller than the second;
+0 if both matrices are equal - this means they have exactly the same values.
+For this task, we assume that a matrix is smaller than another matrix when the first element which is different is smaller in that matrix.
+
+We analyze matrices from left to right and from top to bottom. Separate the declaration of the function from its full definition.
+
+Write a second function that calls the first one and then prints an appropriate message: matrixA is smaller than matrixB, matrixA is greater than matrixB, or Both matrices are equal.
+
+Write a code to test it with some values - create three matrices, fill them with values and compare them in all possible (six) cases. Call a second function. As always, do not limit yourself only to the cases already given.
+
+Note: you can use a single pointer to int type to pass the array.
+
+Expected output
+Both matrices are equal.
+matrixA is smaller than matrixB
+matrixA is greater than matrixB
+Both matrices are equal.
+matrixA is smaller than matrixB
+matrixA is greater than matrixB
+ * @version 0.1
+ * @date 2021-12-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+
+/* your code */
+
+int main()
+{
+	/* your code */
+	return 0;
+}
+/* your code */
