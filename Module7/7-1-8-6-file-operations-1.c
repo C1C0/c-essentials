b2557b29023a401075ed2e35b117800ff3a09044
C1C0 <kpskola1002@gmail.com>
2021-12-11 14:56:42 +0100

    Init

---
+++ Module7/7-1-8-6-file-operations-1.c
@@ -0,0 +1,33 @@
+/**
+ * @file 7-1-8-6-file-operations-1.c
+ * @author your name (you@domain.com)
+ * @brief Scenario
+Write a program that copies itself and adds line numbers to each line.
+ You can (for now, to simplify your task, of course) assume constant file names.
+
+First, open the file in read mode, then open the copy file in write mode.
+ After that, line by line, get the lines from the source file, append the number of the line to the destination file, and append the line content.
+
+Remember to close both files.
+
+Note: in a real program, it's important to check whether all the needed files
+ exist and that you can create a copy. In our program, you should at least check that both files have been successfully opened.
+
+If one of the fopen functions fails, then print the message: Error opening a file. and end the program.
+
+Expected output
+Copy of source file with line numbers.
+ * @version 0.1
+ * @date 2021-12-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	/* your code */
+	return 0;
+}
