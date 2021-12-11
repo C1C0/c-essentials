22a7c21f316fe20df4d9f3d3ab715e1dede551be
C1C0 <kpskola1002@gmail.com>
2021-12-11 14:57:51 +0100

    Init

---
+++ Module7/7-1-8-6-file-operations-2.c
@@ -0,0 +1,67 @@
+/**
+ * @file 7-1-8-6-file-operations-1.c
+ * @author your name (you@domain.com)
+ * @brief Scenario
+Write a program that counts the metrics of a file:
+
+Number of all characters
+Number of whitespaces
+Number of lines
+Number of all lower-case letters
+You can (for now, to simplify your task, of course) assume constant file names. 
+To speed up your programming, you can test your program on its source code. 
+First, open a file in read mode. After that, character by character, read the 
+file and count the numbers asked. Remember to close the file.
+
+Note: in a real program, it's important to check whether all the files needed
+ actually exist and that you can read their contents. In our program, you should 
+ at least check that the files have been successfully opened.
+
+If the fopen function fails, then print the message Error opening file. and end the
+ program. There's only one instance of output from your program - your output can 
+ vary, depending on the file contents.
+
+Expected output
+Lines: 34
+Whitespaces: 153
+Characters: 686
+Small letter: a : 23
+Small letter: b : 1
+Small letter: c : 25
+Small letter: d : 7
+Small letter: e : 27
+Small letter: f : 13
+Small letter: g : 1
+Small letter: h : 12
+Small letter: i : 32
+Small letter: j : 0
+Small letter: k : 1
+Small letter: l : 14
+Small letter: m : 6
+Small letter: n : 36
+Small letter: o : 16
+Small letter: p : 9
+Small letter: q : 0
+Small letter: r : 20
+Small letter: s : 23
+Small letter: t : 39
+Small letter: u : 14
+Small letter: v : 0
+Small letter: w : 4
+Small letter: x : 0
+Small letter: y : 0
+Small letter: z : 3
+ * @version 0.1
+ * @date 2021-12-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	/* your code */
+	return 0;
+}
