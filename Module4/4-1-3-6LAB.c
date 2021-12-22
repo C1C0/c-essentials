6be1d30967065c3ba2b9c8e1dc734eb5bb911eef
C1C0 <kpskola1002@gmail.com>
2021-12-22 13:11:54 +0100

    Example commit.

--- Module4/4-1-3-6LAB.c
+++ Module4/4-1-3-6LAB.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  float numbers[10] = {5.6, 4.3, 6.2, 6.4, 7.3, 2.3, 8.3, 9.2, 0.1, 1.9};
+  float numbers[10] = {5.6, 4.3, 6.2, 6.4, 7.3, 2.3, 8.3, 9.2, 0.1, 2.0};
   /* your code */
   int swapped;
   float aux;
